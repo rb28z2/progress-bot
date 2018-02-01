587513a2784985b4ed7368b74dbe12c0fb518a8f
root <kmarasinghe1@gmail.com>
2018-02-01 00:01:49 +0000

    Moved JS to individual flies and moved stylesheet to end of <body> for faster loading. Should be fully W3C compliant now

---
+++ assets/updater_functions.js
@@ -0,0 +1,95 @@
+	var socket = io();
+
+	console.log("Starting");
+  var stats = {};
+  //var [encode], [title], [episode], [time], [tl], [ts], [edit], [qc];
+
+
+	/**socket.on("irc message", function(msg) {
+		console.log("NEW MESSAGE");
+		$('#messages')
+			.append($('<li>')
+				.text(msg));
+	});*/
+
+
+	socket.on("init-stats", function(val) {
+    if (val["command"] !== "title" && val["command"] !== "episode")
+    {
+			//console.log(val["command"]);
+
+		var dispCommand = "";
+		if (val["command"] === "tl")
+			dispCommand = "Translation";
+		else if (val["command"] === "edit")
+			dispCommand = "Editing";
+		else if (val["command"] === "ts")
+			dispCommand = "Typesetting";
+		else if (val["command"] === "time")
+			dispCommand = "Timing";
+		else if (val["command"] === "encode")
+			dispCommand = "Encoding";
+		else if (val["command"] === "tlc")
+			dispCommand = "TLC";
+		else if (val["command"] === "qc")
+			dispCommand = "QC";
+
+
+
+		stats[val["command"]] = new ProgressBar.Line('#pb-'+val["command"], {
+			easing: 'easeInOut',
+			color: '#287fb8',
+			trailColor: '#555',
+			trailWidth: 1,
+			svgStyle: {width: '100%', height: '50%'},
+			text: {
+				style: {
+					autoStyleContainer: false,
+					color: '#fff',
+					fontFamily: '"Open Sans", Helvetica, Arial, sans-serif',
+					fontSize: '3.4vh',
+					position: 'absolute',
+					width: '100%',
+					textAlign: 'center',
+					fontWeight: '600',
+					top: '0%'
+				}
+			},
+			step: (state, bar) => { bar.setText(dispCommand + ": " + Math.round(bar.value() * 100) + '%');}
+		});
+		stats[val["command"]].animate(val["value"]);
+  }
+  else {
+		console.log(val["value"]);
+		if (val["command"] === "title")
+		{
+    	$("#pb-title").text(val["value"]);
+		}
+		else {
+			$("#pb-episode").text("Episode: " + val["value"]);
+		}
+  }
+	});
+
+
+	socket.on("update-stats", function(val) {
+		if (val["command"] !== "title" && val["command"] !== "episode")
+		{
+			stats[val["command"]].animate(val["value"]/100);
+		}
+		else {
+			if (val["command"] === "title")
+			{
+	    	$("#pb-title").text(val["value"]);
+			}
+			else {
+				$("#pb-episode").text("Episode: " + val["value"]);
+			}
+		}
+	});
+
+	socket.on("update-users", function(val) {
+		$("#totalUsers").text("Users: " + val);
+	});
+
+
---
+++ assets/visual_functions.js
@@ -0,0 +1,31 @@
+$('#plug').hover(function()
+{
+	console.log("Hover");
+	$('#plug-text').fadeIn();
+}, function()
+		{
+			$('#plug-text').stop().delay(3000).fadeOut();
+		}
+);
+
+var original;
+
+$('#pb-qc').hover(function()
+{
+	original = $('#pb-qc > div').text();
+	$('#pb-qc > div').text("Quality Checking");
+}, function()
+	{
+		$('#pb-qc > div').text(original);
+	}
+);
+
+$('#pb-tlc').hover(function()
+{
+	original = $('#pb-tlc > div').text();
+	$('#pb-tlc > div').text("Translation Checking");
+}, function()
+	{
+		$('#pb-tlc > div').text(original);
+	}
+);
