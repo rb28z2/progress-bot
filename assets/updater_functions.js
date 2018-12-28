ea9bf865d8f6aee3bc5b68f5d1633a31f4273a6e
RB28Z2 <kmarasinghe1@gmail.com>
2018-12-28 12:11:49 -0700

    Style changes

--- assets/updater_functions.js
+++ assets/updater_functions.js
@@ -1,14 +1,14 @@
-	var socket = io();
+/* eslint-disable no-undef */
+var socket = io();
 
-	console.log("Starting");
-  var stats = {};
-  //var [encode], [title], [episode], [time], [tl], [ts], [edit], [qc];
+console.log("Starting");
+var stats = {};
+//var [encode], [title], [episode], [time], [tl], [ts], [edit], [qc];
 
 
-	socket.on("init-stats", function(val) {
-    if (val["command"] !== "title" && val["command"] !== "episode")
-    {
-			//console.log(val["command"]);
+socket.on("init-stats", function(val) {
+	if (val["command"] !== "title" && val["command"] !== "episode") {
+		//console.log(val["command"]);
 
 		var dispCommand = "";
 		if (val["command"] === "tl")
@@ -28,65 +28,62 @@
 
 
 
-		stats[val["command"]] = new ProgressBar.Line('#pb-'+val["command"], {
-			easing: 'easeInOut',
-			color: '#287fb8',
-			trailColor: '#555',
+		stats[val["command"]] = new ProgressBar.Line("#pb-" + val["command"], {
+			easing: "easeInOut",
+			color: "#287fb8",
+			trailColor: "#555",
 			trailWidth: 1,
-			svgStyle: {width: '100%', height: '50%'},
+			svgStyle: {
+				width: "100%",
+				height: "50%"
+			},
 			text: {
 				style: {
 					autoStyleContainer: false,
-					color: '#fff',
-					fontFamily: '"Open Sans", Helvetica, Arial, sans-serif',
-					fontSize: '3.4vh',
-					position: 'absolute',
-					width: '100%',
-					textAlign: 'center',
-					fontWeight: '600',
-					top: '0%'
+					color: "#fff",
+					fontFamily: "\"Open Sans\", Helvetica, Arial, sans-serif",
+					fontSize: "3.4vh",
+					position: "absolute",
+					width: "100%",
+					textAlign: "center",
+					fontWeight: "600",
+					top: "0%"
 				}
 			},
-			step: (state, bar) => { bar.setText(dispCommand + ": " + Math.round(bar.value() * 100) + '%');}
+			step: (state, bar) => {
+				bar.setText(dispCommand + ": " + Math.round(bar.value() * 100) + "%");
+			}
 		});
 		stats[val["command"]].animate(val["value"]);
-  }
-  else {
+	} else {
 		console.log(val["value"]);
-		if (val["command"] === "title")
-		{
-    	$("#pb-title").text(val["value"]);
-		}
-		else {
+		if (val["command"] === "title") {
+			$("#pb-title").text(val["value"]);
+		} else {
 			$("#pb-episode").text("Episode: " + val["value"]);
 		}
-  }
-	});
+	}
+});
 
 
-	socket.on("update-stats", function(val) {
-		console.log("Updating");
-		if (val["command"] !== "title" && val["command"] !== "episode")
-		{
-			stats[val["command"]].animate(val["value"]/100);
+socket.on("update-stats", function(val) {
+	console.log("Updating");
+	if (val["command"] !== "title" && val["command"] !== "episode") {
+		stats[val["command"]].animate(val["value"] / 100);
+	} else {
+		if (val["command"] === "title") {
+			$("#pb-title").text(val["value"]);
+		} else {
+			$("#pb-episode").text("Episode: " + val["value"]);
 		}
-		else {
-			if (val["command"] === "title")
-			{
-	    	$("#pb-title").text(val["value"]);
-			}
-			else {
-				$("#pb-episode").text("Episode: " + val["value"]);
-			}
-		}
-        
-	});
+	}
 
-	socket.on("update-users", function(val) {
-		$("#totalUsers").text("Users: " + val);
-	});
+});
 
-	socket.on("date-update", function(val) {
-		$("#update").text(val);
-	});
+socket.on("update-users", function(val) {
+	$("#totalUsers").text("Users: " + val);
+});
 
+socket.on("date-update", function(val) {
+	$("#update").text(val);
+});
--- assets/visual_functions.js
+++ assets/visual_functions.js
@@ -1,31 +1,32 @@
-$('#plug').hover(function()
+/* eslint-disable no-undef */
+$("#plug").hover(function()
 {
 	console.log("Hover");
-	$('#plug-text').fadeIn();
+	$("#plug-text").fadeIn();
 }, function()
-		{
-			$('#plug-text').stop().delay(3000).fadeOut();
-		}
+{
+	$("#plug-text").stop().delay(3000).fadeOut();
+}
 );
 
 var original;
 
-$('#pb-qc').hover(function()
+$("#pb-qc").hover(function()
 {
-	original = $('#pb-qc > div').text();
-	$('#pb-qc > div').text("Quality Checking");
+	original = $("#pb-qc > div").text();
+	$("#pb-qc > div").text("Quality Checking");
 }, function()
-	{
-		$('#pb-qc > div').text(original);
-	}
+{
+	$("#pb-qc > div").text(original);
+}
 );
 
-$('#pb-tlc').hover(function()
+$("#pb-tlc").hover(function()
 {
-	original = $('#pb-tlc > div').text();
-	$('#pb-tlc > div').text("Translation Checking");
+	original = $("#pb-tlc > div").text();
+	$("#pb-tlc > div").text("Translation Checking");
 }, function()
-	{
-		$('#pb-tlc > div').text(original);
-	}
+{
+	$("#pb-tlc > div").text(original);
+}
 );
